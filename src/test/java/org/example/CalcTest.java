6728f49d05e54962c8933656f57bff5c6822e1c6
Shaun-zhang23 <jumbozhang21@outlook.com>
2026-09-23 11:16:19 +0800

    Modify errors

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -13,6 +13,6 @@ public class CalcTest {
     @Test
     void testSubtraction() {
         Calc c = new Calc();
-        assertEquals(3, c.subtract(4, 2));
+        assertEquals(2, c.subtract(4, 2));
     }
 }
